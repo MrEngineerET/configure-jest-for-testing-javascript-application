0c63ccdbacf720babb1237586a75d14f24bbfc38
Biruk Berhanu <meetbirukberhanu@gmail.com>
2022-11-30 11:43:10 +0300

    project initialized with a simple calculator application

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Calculator from './calculator'
+
+function App() {
+  return (
+    <div>
+      <Calculator />
+    </div>
+  )
+}
+
+export default App
+
+/* eslint import/namespace:0 */
